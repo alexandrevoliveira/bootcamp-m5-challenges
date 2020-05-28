0de12d4f9e548ebec52880829d0de53ba680a0d6
Alexandre <alexandrevoliveira30@gmail.com>
2020-05-28 01:45:43 -0300

    pagination algorithm into the frontend

--- public/scripts.js
+++ public/scripts.js
@@ -6,3 +6,40 @@ for (item of menuItems) {
         item.classList.add('active')
     }
 }
+
+
+let totalPages = 17,
+    selectedPage = 13,
+    oldPage,
+    pages = []
+
+
+for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+    if (totalPages > 7) {
+        const firstAndLastPages = currentPage == 1 || currentPage == 2 || currentPage == (totalPages - 1) ||  currentPage == totalPages
+    
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 1
+        const pagesBeforeSelectedPage = currentPage >= selectedPage - 1
+    
+        if (firstAndLastPages || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+    
+            if(oldPage && (currentPage - oldPage) > 2){
+                pages.push("...")
+            }
+    
+            if(oldPage && (currentPage - oldPage) == 2){
+                pages.push(oldPage + 1)
+            }        
+    
+            pages.push(currentPage)
+    
+            oldPage = currentPage
+    
+        }
+    }else {
+        pages.push(currentPage)
+    }
+}
+
+console.log(pages)

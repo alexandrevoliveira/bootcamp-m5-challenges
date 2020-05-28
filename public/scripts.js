0b4463fb6b1f481c47adf47b174654499d3a0424
Alexandre <alexandrevoliveira30@gmail.com>
2020-05-28 19:46:09 -0300

    pagination logic complete and send to the frontend

--- public/scripts.js
+++ public/scripts.js
@@ -8,38 +8,65 @@ for (item of menuItems) {
 }
 
 
-let totalPages = 17,
-    selectedPage = 13,
-    oldPage,
+function paginate(selectedPage, totalPages) {
+    let oldPage,
     pages = []
 
+    for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
 
-for(let currentPage = 1; currentPage <= totalPages; currentPage++) {
-
-    if (totalPages > 7) {
-        const firstAndLastPages = currentPage == 1 || currentPage == 2 || currentPage == (totalPages - 1) ||  currentPage == totalPages
-    
-        const pagesAfterSelectedPage = currentPage <= selectedPage + 1
-        const pagesBeforeSelectedPage = currentPage >= selectedPage - 1
-    
-        if (firstAndLastPages || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
-    
-            if(oldPage && (currentPage - oldPage) > 2){
-                pages.push("...")
+        if (totalPages > 7) {
+            const firstAndLastPages = currentPage == 1 || currentPage == 2 || currentPage == (totalPages - 1) ||  currentPage == totalPages
+        
+            const pagesAfterSelectedPage = currentPage <= selectedPage + 1
+            const pagesBeforeSelectedPage = currentPage >= selectedPage - 1
+        
+            if (firstAndLastPages || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+        
+                if(oldPage && (currentPage - oldPage) > 2){
+                    pages.push("...")
+                }
+        
+                if(oldPage && (currentPage - oldPage) == 2){
+                    pages.push(oldPage + 1)
+                }        
+        
+                pages.push(currentPage)
+        
+                oldPage = currentPage
+        
             }
-    
-            if(oldPage && (currentPage - oldPage) == 2){
-                pages.push(oldPage + 1)
-            }        
-    
+        }else {
             pages.push(currentPage)
-    
-            oldPage = currentPage
-    
         }
-    }else {
-        pages.push(currentPage)
     }
+    return pages
 }
 
-console.log(pages)
+function createPagination(pagination) {
+    const filter = pagination.dataset.filter
+    const page = +pagination.dataset.page
+    const totalPages = +pagination.dataset.totalpages
+    
+    const pages = paginate(page, totalPages)
+    
+    let elements = ""
+    
+    for (let page of pages) {
+        if(String(page).includes("...")){
+            elements += `<span>${page}</span>`
+        } else {
+            if (filter) {
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            } else {
+                elements += `<a href="?page=${page}">${page}</a>`
+            }
+        }
+    }
+    
+    pagination.innerHTML = elements
+}
+const pagination = document.querySelector(".pagination")
+
+if (pagination) {
+    createPagination(pagination)
+}
